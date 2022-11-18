71ff3996db32c679ca269b67cfb6da3d8f174fbb
Edylmorgul <loicpierard@outlook.com>
2022-11-18 17:40:28 +0100

    Project V-8: Intégration de locomotive JS + test fonction changeant le body de couleur

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+// Instanciation de la librairie ==> dispo sur documentation
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    tablet: {smooth: true},
+    smartphone: {smooth:true},
+});
+
+// Changer couleur body
+scroll.on("scroll", () =>{
+    // Si is inview est à l'écran
+    if(document.querySelector("#color.is-inview")){
+        document.body.style.background = "#000101";
+        document.body.style.color = "#fefeff";
+    }
+
+    else{
+        document.body.style.background = "#fefeff";
+        document.body.style.color = "#000101";
+    }
+})
